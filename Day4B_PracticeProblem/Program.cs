d99e11adf0561ffd72fc7e26802999f24e415383
MansiRavindraMohan <manasimohan29@gmail.com>
2023-05-03 19:20:22 +0530

    Add: Program for printing given pattern

---
+++ Day4B_PracticeProblem/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Day4B_PracticeProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+            pattern.DisplayPattern();
+        }
+    }
+}
